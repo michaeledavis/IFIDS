f2ebcba105f6b6f4d6dfba64f069ab8f1040f5ff
Chris King <ccking@smu.edu>
2012-03-05 20:36:44 -0600

    Added license to all source files

--- Module/ifids_module.c
+++ Module/ifids_module.c
@@ -1,3 +1,25 @@
+/*
+ * Intranet Firwall and Intrusion Detection System (IFIDS)    
+ * Copyright (C) 2012  Christopher King, Michael Davis, Sam Doerr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ * For more information, contact Christopher King at ccking@smu.edu
+ *
+ */
+
+
 #define NIPQUAD(addr) \
 	    ((unsigned char *)&addr)[0], \
     ((unsigned char *)&addr)[1], \
